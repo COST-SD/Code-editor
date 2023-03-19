c59326b60f170c368969556fbb2c4701db12c7f5
Aagam Jain <aagamjain573@gmail.com>
2023-03-19 22:16:08 +0530

    init editor

---
+++ src/components/editor.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef } from 'react';
+import Codemirror from 'codemirror';
+import ACTIONS from '../Actions';
+
+const Editor = ({ socketRef, roomId, onCodeChange }) => {
+    const editorRef = useRef(null);
+    useEffect(() => {
+        async function init() {
+            editorRef.current = Codemirror.fromTextArea(
+                document.getElementById('realtimeEditor'),
+                {
+                    mode: { name: 'javascript', json: true },
+                    theme: 'dracula',
+                    autoCloseTags: true,
+                    autoCloseBrackets: true,
+                    lineNumbers: true,
+                }
+            );
+
+            editorRef.current.on('change', (instance, changes) => {
+                const { origin } = changes;
+                const code = instance.getValue();
+                onCodeChange(code);
+                if (origin !== 'setValue') {
+                    socketRef.current.emit(ACTIONS.CODE_CHANGE, {
+                        roomId,
+                        code,
+                    });
+                }
+            });
+        }
+        init();
+    }, []);
+
+    return <textarea id="realtimeEditor"></textarea>;
+};
+
+export default Editor;
